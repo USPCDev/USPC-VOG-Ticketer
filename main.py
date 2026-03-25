bc04d21c19e0d5fd8be90520bafb39da2e22c50f
Geoffry George Mathew <geoffrgeorge.mathew@outlook.com>
2026-03-25 22:28:15 +0000

    app header has been hidden instead of main menu

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     # Hides the streamlit main menu
     st.html("""
                 <style>
-                    MainMenu {visibility: hidden;}
+                    .stAppHeader {visibility: hidden;}
                 </style>
             """)
 
